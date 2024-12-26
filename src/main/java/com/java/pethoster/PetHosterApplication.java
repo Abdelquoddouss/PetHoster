106c14427762b28257e952fbca395f3e4d0100d1
Abdelquoddouss <fadliabdelquoddouss.06@gmail.com>
2024-12-26 09:20:33 +0100

    first commit

---
+++ src/main/java/com/java/pethoster/PetHosterApplication.java
@@ -0,0 +1,13 @@
+package com.java.pethoster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetHosterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetHosterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java/pethoster/PetHosterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.pethoster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetHosterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
