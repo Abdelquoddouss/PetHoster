720f854849f2dcc9c455f53fdf58c91274de72dd
Abdelquoddouss <fadliabdelquoddouss.06@gmail.com>
2025-02-17 14:54:56 +0100

    ajoute package config pour spring security

--- src/main/java/com/java/pethoster/domain/Utilisateur.java
+++ src/main/java/com/java/pethoster/domain/Utilisateur.java
@@ -4,7 +4,11 @@ import com.java.pethoster.domain.enums.Role;
 import com.java.pethoster.domain.enums.TypeAnimal;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 
 @Entity
@@ -14,7 +18,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-public class Utilisateur {
+public class Utilisateur implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -64,4 +68,40 @@ public class Utilisateur {
         this.motDePasse = motDePasse;
     }
 
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return motDePasse;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
